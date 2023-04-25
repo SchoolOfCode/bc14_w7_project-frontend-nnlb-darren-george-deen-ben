0380ffc90706ac2161f5473d9b3c0f302a3e7b02
George <gdo2000@icloud.com>
2023-04-25 16:42:53 +0100

    Ability to cards to deck

--- codecards/src/App.js
+++ codecards/src/App.js
@@ -1,12 +1,42 @@
 import './App.css';
+import { useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Revising from './components/Revising';
+import AddCard from './components/AddCard';
 
 function App() {
+
+  const [deck, setDeck] = useState([]); //state of cards in the deck
+  const [question, setQuestion] = useState(""); //text value of the question input
+  const [answer, setAnswer] = useState(""); //text value of the answer input
+  
+  const questionTextChange = (e) => {
+    setQuestion(e.target.value);
+  } //sets the question state value to the question input value
+
+  const answerTextChange = (e) => {
+    setAnswer(e.target.value);
+  } //sets the answer state value to the answer input value
+
+  const handleAddClick = () => {
+    setDeck([...deck, {question: `${question}`, answer: `${answer}`}]);
+    console.log(deck)
+    setAnswer("");
+    setQuestion("");
+  }// adds the card to the deck
+  console.log(deck)
+
+
+
+
+
+
+
   return (
     <div className="App">
       <Header />
+      <AddCard questionTextChange={questionTextChange} answerTextChange={answerTextChange} handleAddClick={handleAddClick}/>
       <Revising />
       <Footer />
 
--- codecards/src/components/AddCard.js
+++ codecards/src/components/AddCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const AddCard = () => {
+const AddCard = ({questionTextChange, answerTextChange, handleAddClick}) => {
   return (
     <div>
-      <input></input>
-      <input></input>
-      <button></button>
+      <input className='questionText' type='text' placeholder='Enter question here' onChange={questionTextChange}></input>
+      <input className='answerText' type='text' placeholder='Enter answer here' onChange={answerTextChange}></input>
+      <button className='addCard' onClick={handleAddClick}></button>
       <button></button>
     </div>
   )
